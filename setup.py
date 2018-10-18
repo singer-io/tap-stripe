27c74626738510754e3fb00cb223a90714c6bb0c
Eduard Vlasov <e.vlasov@realtimeboard.com>
2018-10-18 15:16:58 +0500

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-stripe",
-    version="0.2.0",
+    version="0.2.1",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",

f5c70d09a458bed8a9be711396ddccea5c5dcd35
Tim Visher <tim@stitchdata.com>
2019-05-07 14:42:51 +0000

    1.2.3 → 1.2.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-stripe",
-    version="1.2.3",
+    version="1.2.4",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",

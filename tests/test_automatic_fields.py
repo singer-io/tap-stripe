4a74032979bd1ffc3481018e4e60977097cd07d4
Kyle Speer <54034650+kspeer825@users.noreply.github.com>
2020-03-31 10:11:35 -0400

    Update/retry logic in tests (#55)
    * add retry logic to tests
    
    * uncomment assertions in auto fields test
    
    * add a pause before the failing source creation
    
    * don't test disputes or transfers
    
    Co-authored-by: Kyle Speer <kspeer@stitchdata.com>

--- tests/test_automatic_fields.py
+++ tests/test_automatic_fields.py
@@ -23,6 +23,11 @@ class MinimumSelectionTest(BaseTapTest):
         fetch of data.  For instance if you have a limit of 250 records ensure
         that 251 (or more) records have been posted for that stream.
         """
+        untested_streams = {
+            "disputes",
+            "transfers",
+        }
+
         # Select all streams and no fields within streams
         # IF THERE ARE NO AUTOMATIC FIELDS FOR A STREAM
         # WE WILL NEED TO UPDATE THE BELOW TO SELECT ONE
@@ -34,25 +39,25 @@ class MinimumSelectionTest(BaseTapTest):
 
         actual_fields_by_stream = runner.examine_target_output_for_fields()
 
-        for stream in self.expected_streams():
+        for stream in self.expected_streams().difference(untested_streams):
             with self.subTest(stream=stream):
 
                 # verify that you get more than a page of data
                 # SKIP THIS ASSERTION FOR STREAMS WHERE YOU CANNOT GET
                 # MORE THAN 1 PAGE OF DATA IN THE TEST ACCOUNT
-                #self.assertGreater(
-                    #record_count_by_stream.get(stream, -1),
-                    #self.expected_metadata().get(stream, {}).get(self.API_LIMIT, 0),
-                    #msg="The number of records is not over the stream max limit")
+                self.assertGreater(
+                    record_count_by_stream.get(stream, -1),
+                    self.expected_metadata().get(stream, {}).get(self.API_LIMIT, 0),
+                    msg="The number of records is not over the stream max limit")
 
                 # verify that only the automatic fields are sent to the target
                 actual = actual_fields_by_stream.get(stream) or set()
                 expected = self.expected_automatic_fields().get(stream, set())
-                #self.assertEqual(
-                    #actual, expected,
-                    #msg=("The fields sent to the target are not the automatic fields. Expected: {}, Actual: {}"
-                         #.format(actual, expected))
-                #)
+                self.assertEqual(
+                    actual, expected,
+                    msg=("The fields sent to the target are not the automatic fields. Expected: {}, Actual: {}"
+                         .format(actual, expected))
+                )
 
 
 SCENARIOS.add(MinimumSelectionTest)

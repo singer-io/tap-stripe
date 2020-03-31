24f66c1641ef5c0acf43dfe05078c0813ef159fa
Kyle Speer <kspeer@stitchdata.com>
2020-03-31 09:23:28 -0400

    fix branch, mark disputes/transfers untested

--- tests/test_automatic_fields.py
+++ tests/test_automatic_fields.py
@@ -23,6 +23,11 @@ class MinimumSelectionTest(BaseTapTest):
         fetch of data.  For instance if you have a limit of 250 records ensure
         that 251 (or more) records have been posted for that stream.
         """
+        untested_streams = {
+            "disputes",
+            "transfers",
+        }
+
         # Select all streams and no fields within streams
         # IF THERE ARE NO AUTOMATIC FIELDS FOR A STREAM
         # WE WILL NEED TO UPDATE THE BELOW TO SELECT ONE
@@ -40,19 +45,19 @@ class MinimumSelectionTest(BaseTapTest):
                 # verify that you get more than a page of data
                 # SKIP THIS ASSERTION FOR STREAMS WHERE YOU CANNOT GET
                 # MORE THAN 1 PAGE OF DATA IN THE TEST ACCOUNT
-                #self.assertGreater(
-                    #record_count_by_stream.get(stream, -1),
-                    #self.expected_metadata().get(stream, {}).get(self.API_LIMIT, 0),
-                    #msg="The number of records is not over the stream max limit")
+                self.assertGreater(
+                    record_count_by_stream.get(stream, -1),
+                    self.expected_metadata().get(stream, {}).get(self.API_LIMIT, 0),
+                    msg="The number of records is not over the stream max limit")
 
                 # verify that only the automatic fields are sent to the target
                 actual = actual_fields_by_stream.get(stream) or set()
                 expected = self.expected_automatic_fields().get(stream, set())
-                #self.assertEqual(
-                    #actual, expected,
-                    #msg=("The fields sent to the target are not the automatic fields. Expected: {}, Actual: {}"
-                         #.format(actual, expected))
-                #)
+                self.assertEqual(
+                    actual, expected,
+                    msg=("The fields sent to the target are not the automatic fields. Expected: {}, Actual: {}"
+                         .format(actual, expected))
+                )
 
 
 SCENARIOS.add(MinimumSelectionTest)

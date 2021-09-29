8dbd50d7e1340ffbb7d37123b908c5c4f5ee2d0b
kspeer <kspeer@talend.com>
2021-09-29 14:37:38 +0000

    update the "all tests running" test to use unittest

--- tests/test_all_tests_run.py
+++ tests/test_all_tests_run.py
@@ -1,3 +1,4 @@
+import unittest
 import os
 
 potential_paths = [
@@ -19,30 +20,34 @@ def go_to_tests_directory():
 ### TEST
 ##########################################################################
 
+class TestingTests(unittest.TestCase):
+    def test_regression_suite(self):
+        print("Acquiring path to tests directory.")
+        cwd = go_to_tests_directory()
 
-print("Acquiring path to tests directory.")
-cwd = go_to_tests_directory()
+        print("Reading in filenames from tests directory.")
+        files_in_dir = os.listdir(cwd)
 
-print("Reading in filenames from tests directory.")
-files_in_dir = os.listdir(cwd)
+        print("Dropping files that are not of the form 'test_<feature>.py'.")
+        test_files_in_dir = [fn for fn in files_in_dir if fn.startswith('test_') and fn.endswith('.py')]
 
-print("Dropping files that are not of the form 'test_<feature>.py'.")
-test_files_in_dir = [fn for fn in files_in_dir if fn.startswith('test_') and fn.endswith('.py')]
+        print("Files found: {}".format(test_files_in_dir))
 
-print("Files found: {}".format(test_files_in_dir))
+        print("Reading contents of circle config.")
+        with open(cwd + "/../.circleci/config.yml", "r") as config:
+            contents = config.read()
 
-print("Reading contents of circle config.")
-with open(cwd + "/../.circleci/config.yml", "r") as config:
-    contents = config.read()
+        print("Parsing circle config for run blocks.")
+        runs = contents.replace(' ', '').replace('\n', '').split('-run_integration_test:')
 
-print("Parsing circle config for run blocks.")
-runs = contents.replace(' ', '').replace('\n', '').split('-run_integration_tests:')
+        print("Verify all test files are executed in circle...")
+        tests_not_found = set(test_files_in_dir)
+        for filename in test_files_in_dir:
+            print("\tVerifying {} is running in circle.".format(filename))
+            trimmed_file_name = filename.split('.')[0].split('test_')[1]
+            file_param = f"file:{trimmed_file_name}"
+            if any([file_param in run for run in runs]):
+                tests_not_found.remove(filename)
+        self.assertSetEqual(tests_not_found, set())
+        print("\t SUCCESS: All tests are running in circle.")
 
-print("Verify all test files are executed in circle...")
-tests_not_found = set(test_files_in_dir)
-for filename in test_files_in_dir:
-    print("\tVerifying {} is running in circle.".format(filename))
-    if any([filename in run for run in runs]):
-        tests_not_found.remove(filename)
-assert tests_not_found == set(), "The following tests are not running in circle:\t{}".format(tests_not_found)
-print("\t SUCCESS: All tests are running in circle.")

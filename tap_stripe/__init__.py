b17779242d7ded56b321ddaf7563588557e74f9a
Noah Schairer <nschairer@gmail.com>
2021-12-02 14:47:02 -0500

    logging

--- tap_stripe/__init__.py
+++ tap_stripe/__init__.py
@@ -402,6 +402,7 @@ def sync_stream(stream_name):
 
     stream_metadata        = metadata.to_map(Context.get_catalog_entry(stream_name)['metadata'])
     stream_query_params    = Context.get_catalog_entry(stream_name).get('query')
+    LOGGER.info("QUERY PARAMS: ", stream_query_params)
     stream_field_whitelist = json.loads(Context.config.get('whitelist_map', '{}')).get(stream_name)
 
     extraction_time = singer.utils.now()

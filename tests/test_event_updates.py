a562c7835b6079841441d77caf33c753385122c6
prijendev <prijen.khokhani@crestdatasys.com>
2022-02-18 15:02:56 +0530

    Added test cases for event updates

--- tests/test_event_updates.py
+++ tests/test_event_updates.py
@@ -28,6 +28,7 @@ class EventUpdatesTest(BaseTapTest):
         Verify that the second sync includes at least one update for each stream
         Verify that the second sync includes less records than the first sync
         Verify that the updated metadata was picked up on the second sync
+        Verify that the `event_type` field is available in all records emitted by event_updates
 
         PREREQUISITE
         For EACH stream that gets updates through events stream, there's at least 1 row
@@ -165,6 +166,14 @@ class EventUpdatesTest(BaseTapTest):
                     first_data["metadata"].get("test_value", 0),
                     "the test metadata should be different",
                 )
+
+                # Verify that the `event_type` field is available in all records emitted by event_updates
+                for message in second_sync_updated.get(stream, {}).get("messages", []):
+                    if message['action'] == 'upsert':
+                        self.assertIn(
+                            'event_type', 
+                            message['data'].keys(),
+                            "event_type field is missing in event_updates records")
 
                 if stream in new_objects:
                     delete_object(stream, new_objects[stream]["id"])

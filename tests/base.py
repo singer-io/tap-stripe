56eccce4d384222266f021b1398095fbf3c5a11a
prijendev <prijen.khokhani@crestdatasys.com>
2022-03-01 15:13:59 +0530

    Resloved all fields test case error

--- tests/base.py
+++ tests/base.py
@@ -392,7 +392,7 @@ class BaseTapTest(unittest.TestCase):
             ]
             
             object_keys = [
-                'discount', 'plan', 'coupon', 'status_transitions', 'period', 'sources', 'source', 'refunds',
+                'discount', 'plan', 'coupon', 'status_transitions', 'period', 'sources', 'source', 'charges,' 'refunds',
                 'package_dimensions', 'price' # Convert epoch timestamp value of 'price.created' to standard datetime format. This field is available specific for invoice_line_items stream
             ]
 

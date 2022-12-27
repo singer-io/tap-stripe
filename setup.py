b1dbc72b49a3678cb609bf9c5bcfc1378398e828
Rushikesh Todkar <98420315+RushiT0122@users.noreply.github.com>
2022-12-27 15:26:34 +0530

    bump version 2.0.2 (#163)
    Co-authored-by: RushiT0122 <rtodkar@stitchdata-talend.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-stripe",
-    version="2.0.1",
+    version="2.0.2",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",

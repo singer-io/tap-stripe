79e89b7608b24ad44af560d13b64a1326fe681e6
RushiT0122 <rtodkar@stitchdata-talend.com>
2022-12-27 12:05:21 +0000

    * revert singer-python version * update changelog * bump tap version

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 
 setup(
     name="tap-stripe",
-    version="2.0.1",
+    version="2.0.3",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_stripe"],
     install_requires=[
-        "singer-python==5.13.0",
+        "singer-python==5.5.1",
         "stripe==2.64.0",
     ],
     extras_require={

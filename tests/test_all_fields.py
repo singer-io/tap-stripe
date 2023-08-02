e44d6cc0a42b9977b6ba1bdccddcad8e29f76a4d
Rutuja Deshmukh <rdeshmukh@talend.com>
2023-08-02 07:35:32 +0000

    fixed test_all_fields and removed refunds from charge.json

--- tests/test_all_fields.py
+++ tests/test_all_fields.py
@@ -52,6 +52,16 @@ KNOWN_MISSING_FIELDS = {
     'payment_intents': set()
 }
 
+# Fields which are deprecated and no longer will come in the response of api
+deprecated_fields =  {
+    'charges': {
+        'refunds'
+    },
+    'payment_intents': {
+        'charges'
+    }
+}
+
 # we have observed that the SDK object creation returns some new fields intermittently, which are not present in the schema
 SCHEMA_MISSING_FIELDS = {
     'customers': {
@@ -469,10 +479,9 @@ class ALlFieldsTest(BaseTapTest):
                 actual_records_keys = actual_records_keys - FIELDS_TO_NOT_CHECK[stream]
                 expected_records_keys = expected_records_keys - FIELDS_TO_NOT_CHECK[stream]
 
-                # Append fields which are added by tap to expectation
+                # Append fields which are added by tap to expectation - deprecated_fields
                 adjusted_expected_keys = expected_records_keys.union(
-                    FIELDS_ADDED_BY_TAP.get(stream, set())
-                )
+                    FIELDS_ADDED_BY_TAP.get(stream, set())) - deprecated_fields.get(stream, set())
 
                 # Log the fields that are included in the schema but not in the expectations.
                 # These are fields we should strive to get data for in our test data set
